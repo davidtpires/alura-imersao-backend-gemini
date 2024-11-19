88c100f6b60ba26cdd0ed6c3e3b0e1bd34f85621
David T. <davidt.info@gmail.com>
2024-11-19 09:43:47 -0300

    Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Foto de uma paisagem natural",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Gráfico de barras representando vendas",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Desenho animado de um gato",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Imagem de um produto em e-commerce",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 6,
+        descricao: "Icone de uma rede social",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+app.get("/posts", (req, res) => {
+    res.status(200).send(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).send(posts[index]);
 });
